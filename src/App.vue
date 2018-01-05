b9fdd5221ffce30c724795d3c80ba18b42e0699a
Mrugank <mrugank2@gmail.com>
2018-01-04 20:33:51 -0500

    fixing ampersand sign

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <v-toolbar class="primary" dark>
       <v-toolbar-title>MSA</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items >
         <v-btn flat href="/">Home</v-btn>
         <v-btn flat href="/Meetings">Meetings</v-btn>
-        <v-btn flat href="/QandA">Q&A</v-btn>
+        <v-btn flat href="/QandA">Q&#x26A</v-btn>
         <v-btn flat href="/SignUp">SignUp</v-btn>
       </v-toolbar-items>
     </v-toolbar>
